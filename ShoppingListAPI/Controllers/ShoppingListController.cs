1e63827f859d50583dc5bd36dfffbf9f41af4c82
Manuel Valtingojer <manuel.va@gmx.net>
2023-05-05 15:26:11 +0200

    Implement Route with Get

---
+++ ShoppingListAPI/Controllers/ShoppingListController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShoppingListAPI.Controllers
+{
+    [Route("[controller]/api/")] // https://...//shoppinglist/api/
+    [ApiController]
+    public class ShoppingListController: ControllerBase
+    {
+        #region // https://.../shoppinglist/api
+        /// <summary>
+        ///  Method to check if webservice is running
+        /// </summary>
+        /// <returns>return an information</returns>
+        [HttpGet]
+        [ProducesResponseType(typeof(string), 200)]
+        [ProducesResponseType(typeof(string), 404)]
+        [ProducesResponseType(typeof(string), 500)]
+        public string Get()
+        {
+            return "*** ShoppingList REST API is running...";
+        }
+
+        #endregion
+    }
+}
---
+++ ShoppingListAPI/Models/Article.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingListAPI.Models
+{
+    public class Article
+    {
+        public long ID { get; set; }
+        public string Name { get; set; }
+        public string Amount { get; set; }
+        public string Remark { get; set; } 
+        public bool IsBought { get; set; }
+    }
+}
