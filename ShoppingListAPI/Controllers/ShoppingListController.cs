52f478415e8e4f2dd355c15daf8282082df312cd
Manuel Valtingojer <manuel.va@gmx.net>
2023-05-05 17:48:21 +0200

    Implement POst

--- ShoppingListAPI/Controllers/ShoppingListController.cs
+++ ShoppingListAPI/Controllers/ShoppingListController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingListAPI.Models;
 
 namespace ShoppingListAPI.Controllers
 {
@@ -6,6 +7,8 @@ namespace ShoppingListAPI.Controllers
     [ApiController]
     public class ShoppingListController: ControllerBase
     {
+        private List<Article> shoppingArticles = new List<Article>();
+
         #region // https://.../shoppinglist/api
         /// <summary>
         ///  Method to check if webservice is running
@@ -18,6 +21,45 @@ namespace ShoppingListAPI.Controllers
         public string Get()
         {
             return "*** ShoppingList REST API is running...";
+        }
+
+        #endregion
+
+        #region // https://...//shoppinglist/api/additem
+        /// <summary>
+        /// Method to add a new article
+        /// </summary>
+        /// <param name="article">Specified an object of type 'Article'.</param>
+        /// <returns>ResultCode</returns>
+        [ProducesResponseType(typeof(string), 200)]
+        [ProducesResponseType(typeof(string), 201)]
+        [ProducesResponseType(typeof(string), 400)]
+        [ProducesResponseType(typeof(string), 409)]
+        [ProducesResponseType(typeof(string), 500)]
+        [HttpPost("additem",Name = "PostAddItem")]
+        public IActionResult PostAddItem([FromBody]Article article)
+        {
+            try
+            {
+                if (article == null)
+                {
+                    return BadRequest(ModelState); // equals statuscode 400
+                }
+
+                if (shoppingArticles.Exists(x=> x.Name == article.Name))
+                {
+                    ModelState.AddModelError("", "The specified article still exists!");
+                    return StatusCode(409, ModelState);// 409 equals 'ClientConflict'
+                }
+                shoppingArticles.Add(article);
+
+                //return Ok(); // equals statuscode 200
+                return StatusCode(201, article); // 201 equals 'Created'
+            }
+            catch (Exception exception)
+            {
+                return StatusCode(500, "Internal error occured:" + exception.Message + " InnerException:" + exception.InnerException);
+            }
         }
 
         #endregion
